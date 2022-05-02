9511cda426b1819383fedd188b6e26e7bd3623d4
RAHUL RUDRA <rahulrudra245@gmail.com>
2022-05-02 11:05:34 +0600

    reactformadded

---
+++ src/Pages/AddService/Add.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { useForm } from "react-hook-form";
+
+const Add = () => {
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data) => console.log(data);
+  return (
+    <div className="mx-auto w-50">
+      <h2>Please Add Service</h2>
+
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input
+          className="mb-2"
+          placeholder="Nmae"
+          {...register("name", { required: true, maxLength: 20 })}
+        />
+        <input
+          className="mb-2"
+          placeholder="Desccription"
+          {...register("description")}
+        />
+        <input
+          className="mb-2"
+          placeholder="Price"
+          type="number"
+          {...register("price")}
+        />
+        <input
+          className="mb-2"
+          placeholder="Photo Url"
+          type="text"
+          {...register("img")}
+        />
+        <input className="mb-2" placeholder="" type="submit" />
+      </form>
+    </div>
+  );
+};
+
+export default Add;
